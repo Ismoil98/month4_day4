bd087583d23605cd89272bac69515a378c62ffc4
Ismoil98 <ismoil6366>
2024-01-08 10:53:38 +0500

    changed

--- lesson/main.js
+++ lesson/main.js
@@ -109,7 +109,86 @@
 // console.log(s);
 
 
+// for15
+// let n = +prompt("n=")
+// let s=1
+// let S=0
+// for (let i=1; i<=n; i++){
+//   s *= i;
+//   console.log(s);
+//   S += s;
+// }
+// console.log(S);
 
+
+
+// for16
+// let n = +prompt("n=")
+// let k = +prompt("k=")
+// let s=1
+// let S=0
+// for (let i=1; i<=n; i++) {
+//   for (let j=1; j<=k; j++) {
+//     s *= i;
+//   }
+//   S += s;
+//   s = 1
+// }
+// console.log(S);
+
+
+// for17
+// let n = +prompt("n=")
+// let s=1
+// let S=0
+// for (let i=1; i<=n; i++) {
+//   for (let j=1; j<=i; j++) {
+//     s *= i;
+//   }
+//   console.log(s);
+//   S += s;
+//   s = 1;
+// }
+// console.log(S);
+
+
+// for18 
+// let n = +prompt('n=')
+// s = 0
+// counter = 0
+// for (let i=1; i<=n; i++) {
+//   if (n%i==0) {
+//     counter ++;
+//     s += i;
+//     console.log(i);
+//   }
+// }
+// console.log(s);
+// console.log(counter);
+
+
+// let19
+// let n = +prompt('n=')
+// counter = 0
+// for (let i=2; i<n; i++) {
+//   if (n%i==0) {
+//     counter ++;
+//   }
+// }
+// if (counter != 0) {
+//   console.log(`${n} tub son emas`);
+// } else {
+//   console.log(`${n} tub son`);
+// }
+
+
+// for20 
+let n = +prompt('n=')
+for (let i=1; i<=n; i++) {
+  for (let j=1; j<=i; j++) {
+    console.log(j);
+  }
+}
 
 // whiln1
 // let a = +prompt('a=')
