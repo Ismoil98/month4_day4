7dd1f85cdb7f7ec848c1980074e47cbd3be822b9
Ismoil98 <ismoil6366>
2024-01-08 10:25:02 +0500

    changed

--- lesson/main.js
+++ lesson/main.js
@@ -32,10 +32,83 @@
 // let a = +prompt("a=")
 // let b = +prompt("b=")
 // let s = 0;
-
 // for (let i=a; i<=b; i++) {
 //     s += i;
 // }
+// console.log(s);
+
+
+// for6 
+// let a = +prompt("a=")
+// let b = +prompt("b=")
+// let s = 1;
+// for (let i=a; i<=b; i++) {
+//     s *= i;
+// }
+// console.log(s);
+
+// for7 
+// let a = +prompt("a=")
+// let b = +prompt("b=")
+// let s = 0;
+// for (let i=a; i<=b; i++) {
+//     s += i*i;
+// }
+// console.log(s);
+
+// for8 
+// let n = +prompt('n=')
+// let s = 0
+// for (let i=1; i<=n; i ++) {
+//   s += 1/i;
+// }
+// console.log(s);
+
+// for9 
+// let n = +prompt("n=")
+// let s = 1;
+// for (let i=1.1; i<=n; i+=0.1) {
+//     s *= i;
+// }
+// console.log(s);
+
+// for11 
+// let n = +prompt("n=")
+// let s = 0;
+// for (let i=1; i<=n; i++) {
+//     s += 2*i-1;
+//     console.log(s);
+// }
+
+// for12 
+// let a = +prompt("a=")
+// let n = +prompt("n=")
+// let s = 1;
+// for (let i=1; i<=n; i++) {
+//     s *= a;
+//   }
+// console.log(s);
+
+
+// for13 
+// let a = +prompt("a=")
+// let n = +prompt("n=")
+// for (let i=1; i<=n; i*=a) {
+//     console.log(i);
+// }
+
+
+// for14
+// let a = +prompt("a=")
+// let n = +prompt("n=")
+// let s = 0;
+// for (let i=1; i<=n; i*=a) {
+//     console.log(i);
+//     s += i;
+// }
+// console.log(s);
+
+
 
 
 // whiln1
@@ -57,17 +130,17 @@
 // }
 // console.log(`${s} ta kesma joylashtirish mumkin`);
 
-let n = +prompt("n=")
-let x = 3;
-let i = 1;
+// let n = +prompt("n=")
+// let x = 3;
+// let i = 1;
 
-while (x<=n) {
-  if (x==n) {
-    console.log(`${n} 3 ning ${i} darajasi`);
-    x *=3;
-    i ++;
-  }
-}
+// while (x<=n) {
+//   if (x==n) {
+//     console.log(`${n} 3 ning ${i} darajasi`);
+//     x *=3;
+//     i ++;
+//   }
+// }
 
 
 
